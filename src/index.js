cce243510138649ecc29cc5e092c6b7d991ca78a
Madhurjai <madhurjaiswal@cedcoss.com>
2022-02-05 11:30:16 +0530

    add two variable

--- src/index.js
+++ src/index.js
@@ -13,19 +13,22 @@ console.log(c)
 // Understanding Case Sensitivity in Variables 
 
 // myvar and MyVar both are different
-let a = 20 ;
-{
-    let a= 10 ;
-    console.log(a) ;
-    // o/p = 10
-}
-console.log(a);
-// o/p = 20 
+// let a = 20 ;
+// {
+//     let a= 10 ;
+//     console.log(a) ;
+//     // o/p = 10
+// }
+// console.log(a);
+// // o/p = 20 
 
-{
-    console.log(temp) ;
-    // o/p = 10
+// {
+//     console.log(temp) ;
+//     // o/p = 10
 
-}
-const name ;
+// }
+const name = 'MAD';
 // its value can't be changed
+var a = 10 ;
+var b = 20 ;
+console.log(a +b); 
