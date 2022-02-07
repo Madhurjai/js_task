af1c043811221659748f10504fef62df13b2d03b
Madhurjai <madhurjaiswal@cedcoss.com>
2022-02-07 09:44:40 +0530

    compare with greater than equal operator

--- src/index1.js
+++ src/index1.js
@@ -216,3 +216,17 @@ console.log(undefine()) ; //it return undefined
   }
   
   testGreaterThan(10);
+
+  function testGreaterThan_equal(val) {
+    if (val >= 100) {  // Change this line
+      return "Over 100";
+    }
+  
+    if (val >= 10) {  // Change this line
+      return "Over 10";
+    }
+  
+    return "10 or Under";
+  }
+  
+  testGreaterThan_equal(10);
