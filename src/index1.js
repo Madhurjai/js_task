cff5095dee518df5bfd513382f579070ba4485d4
Madhurjai <madhurjaiswal@cedcoss.com>
2022-02-07 09:37:22 +0530

    ineqality operator

--- src/index1.js
+++ src/index1.js
@@ -184,3 +184,12 @@ console.log(undefine()) ; //it return undefined
   else{
       console.log(true);
   }
+
+  function testNotEqual(val) {
+    if (val != 99) { // Change this line
+      return "Not Equal";
+    }
+    return "Equal";
+  }
+  
+  testNotEqual(10);
