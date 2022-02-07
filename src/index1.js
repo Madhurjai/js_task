5bd2c5476b7c4ab39d6dacad02a255e2d2680e98
Madhurjai <madhurjaiswal@cedcoss.com>
2022-02-07 09:46:33 +0530

    compare with less than  operator

--- src/index1.js
+++ src/index1.js
@@ -230,3 +230,17 @@ console.log(undefine()) ; //it return undefined
   }
   
   testGreaterThan_equal(10);
+
+  function testLessThan(val) {
+    if (val < 25) {  // Change this line
+      return "Under 25";
+    }
+  
+    if (val < 55) {  // Change this line
+      return "Under 55";
+    }
+  
+    return "55 or Over";
+  }
+  
+  testLessThan(10);
